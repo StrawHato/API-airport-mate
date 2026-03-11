db9fa900aa5b17fa50a454633c29c622148b6068
Maksym Pakula <gargantadoto@gmail.com>
2026-03-11 18:37:13 +0200

    Add Crew serializer

--- airport/serializers.py
+++ airport/serializers.py
@@ -3,6 +3,7 @@ from rest_framework import serializers
 from airport.models import (
     AirplaneType,
     Airplane,
+    Crew,
 )
 
 
@@ -28,3 +29,9 @@ class AirplaneSerializer(serializers.ModelSerializer):
             "airplane_type",
             "capacity"
         )
+
+
+class CrewSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Crew
+        fields = ("id", "first_name", "last_name", "full_name")
