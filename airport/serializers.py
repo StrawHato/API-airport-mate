12d4584c9682d1773f3856901bd2709ac1e0caf5
Maksym Pakula <gargantadoto@gmail.com>
2026-03-11 18:55:50 +0200

    Add Order serializer

--- airport/serializers.py
+++ airport/serializers.py
@@ -1,3 +1,4 @@
+from django.db import transaction
 from rest_framework import serializers
 from rest_framework.exceptions import ValidationError
 
@@ -7,7 +8,9 @@ from airport.models import (
     Crew,
     Airport,
     Route,
-    Flight, Ticket,
+    Flight,
+    Ticket,
+    Order,
 )
 
 
@@ -82,3 +85,19 @@ class TicketSerializer(serializers.ModelSerializer):
             ValidationError
         )
         return data
+
+
+class OrderSerializer(serializers.ModelSerializer):
+    tickets = TicketSerializer(many=True, read_only=False, allow_empty=False)
+
+    class Meta:
+        model = Order
+        fields = ("id", "tickets", "created_at")
+
+    def create(self, validated_data):
+        with transaction.atomic():
+            tickets_data = validated_data.pop("tickets")
+            order = Order.objects.create(**validated_data)
+            for ticket in tickets_data:
+                Ticket.objects.create(order=order, **ticket)
+            return order
