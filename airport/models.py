430283e63790651c0ae98a2d1b2708319855de94
Maksym Pakula <gargantadoto@gmail.com>
2026-03-11 18:24:34 +0200

    Add Ticket model

--- airport/models.py
+++ airport/models.py
@@ -1,4 +1,5 @@
 from django.conf import settings
+from django.core.exceptions import ValidationError
 from django.db import models
 from django.db.models import Q, F
 
@@ -62,7 +63,7 @@ class Route(models.Model):
     distance = models.IntegerField()
 
     def __str__(self):
-        return f"You are flying from {self.source} to {self.destination}"
+        return f"You are flying from {self.source.name} to {self.destination.name}"
 
     class Meta:
         constraints = [
@@ -92,7 +93,7 @@ class Flight(models.Model):
     arrival_time = models.DateTimeField()
 
     def __str__(self):
-        return f"{self.route} on {self.airplane}"
+        return f"{str(self.route)} on {str(self.airplane)}"
 
     class Meta:
         ordering = ["departure_time"]
@@ -111,3 +112,71 @@ class Order(models.Model):
 
     class Meta:
         ordering = ["-created_at"]
+
+
+class Ticket(models.Model):
+    row = models.IntegerField()
+    seat = models.IntegerField()
+    flight = models.ForeignKey(
+        Flight,
+        on_delete=models.CASCADE,
+        related_name="tickets"
+    )
+    order = models.ForeignKey(
+        Order,
+        on_delete=models.CASCADE,
+        related_name="tickets"
+    )
+
+    @staticmethod
+    def validate_ticket(row, seat, airplane, error_to_raise):
+        for ticket_attr_value, ticket_attr_name, airplane_attr_name in [
+            (row, "row", "rows"),
+            (seat, "seat", "seats_in_row"),
+        ]:
+            count_attrs = getattr(airplane, airplane_attr_name)
+            if not (1 <= ticket_attr_value <= count_attrs):
+                raise error_to_raise(
+                    {
+                        ticket_attr_name: f"{ticket_attr_name} "
+                        f"number must be in available range: "
+                        f"(1, {airplane_attr_name}): "
+                        f"(1, {count_attrs})"
+                    }
+                )
+
+    def clean(self):
+        Ticket.validate_ticket(
+            self.row,
+            self.seat,
+            self.flight.airplane,
+            ValidationError,
+        )
+
+    def save(
+        self,
+        *args,
+        force_insert=False,
+        force_update=False,
+        using=None,
+        update_fields=None,
+    ):
+        self.full_clean()
+        return super(Ticket, self).save(
+            force_insert, force_update, using, update_fields
+        )
+
+
+    def __str__(self):
+        return (
+            f"Flight: {str(self.flight)}(row: {self.row}, seat: {self.seat})"
+        )
+
+    class Meta:
+        ordering = ["row", "seat"]
+        constraints = [
+            models.UniqueConstraint(
+                fields=["flight", "row", "seat"],
+                name="unique_ticket_flight",
+            )
+        ]
